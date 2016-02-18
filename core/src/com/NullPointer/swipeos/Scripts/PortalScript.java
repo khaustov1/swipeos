484daef0c5a2128efd3659c24e7aa5cbf47891ed
Kirill Khaustov <la2haste@gmail.com>
2016-02-18 21:43:34 +0300

    Удалил ненужный код из скрипта портала

--- core/src/com/NullPointer/swipeos/Scripts/PortalScript.java
+++ core/src/com/NullPointer/swipeos/Scripts/PortalScript.java
@@ -12,27 +12,18 @@ import com.uwsoft.editor.renderer.utils.ComponentRetriever;
 public class PortalScript implements IScript {
     TransformComponent transformComponent;
     DimensionsComponent dimensionsComponent;
-    float portalRadius;
-    double centerX, centerY;
 
     @Override
     public void init(Entity entity) {
         transformComponent = ComponentRetriever.get(entity,TransformComponent.class);
         dimensionsComponent = ComponentRetriever.get(entity, DimensionsComponent.class);
-        portalRadius = (float) (Math.sqrt((dimensionsComponent.width/2)*dimensionsComponent.width/2)
-                        + (dimensionsComponent.height/2)*(dimensionsComponent.height/2));
-        portalRadius = 10;
-        centerX = transformComponent.x+dimensionsComponent.width/2;
-        centerY = transformComponent.y-dimensionsComponent.height/2;
         transformComponent.scaleX = 2f;
         transformComponent.scaleY = 2f;
     }
 
     @Override
     public void act(float delta) {
-        //transformComponent.x = (float) (centerX + portalRadius * Math.cos(angle));
-        //transformComponent.y = (float) (centerY + portalRadius*Math.sin(angle));
-        transformComponent.rotation += 40 *delta;
+        transformComponent.rotation -= 40 *delta;
     }
 
     @Override
