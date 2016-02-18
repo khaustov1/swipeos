d314ad0195cc8caa6876166607c7bdfa03f26dfb
Kirill Khaustov <la2haste@gmail.com>
2016-02-18 16:33:20 +0300

    Добавлена возможность изменения положения стен

--- core/src/com/NullPointer/swipeos/Objects/Wall.java
+++ core/src/com/NullPointer/swipeos/Objects/Wall.java
@@ -1,19 +1,50 @@
 package com.NullPointer.swipeos.Objects;
 
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.math.Rectangle;
+import com.uwsoft.editor.renderer.components.DimensionsComponent;
+import com.uwsoft.editor.renderer.components.TransformComponent;
+import com.uwsoft.editor.renderer.utils.ComponentRetriever;
 
 /**
  * Created by Khaustov on 17.02.16.
  */
 public class Wall  implements GameObject{
     private Rectangle wallRectangle;
+    private TransformComponent transformComponent;
+    private DimensionsComponent dimensionsComponent;
 
-    public  Wall(float x, float y, float width, float height){
-        wallRectangle = new Rectangle(x, y, width, height);
+    public  Wall(Entity entity){
+        transformComponent = ComponentRetriever.get(entity, TransformComponent.class);
+        dimensionsComponent = ComponentRetriever.get(entity, DimensionsComponent.class);
+        wallRectangle = new Rectangle(transformComponent.x, transformComponent.y,
+                dimensionsComponent.width, dimensionsComponent.height);
     }
 
     public Rectangle getWallRectangle(){
         return wallRectangle;
+    }
+
+    public void setX(float x){
+        transformComponent.x = x;
+        wallRectangle.setX(x);
+    }
+
+    public void setY(float y){
+        transformComponent.y = y;
+        wallRectangle.setY(y);
+    }
+
+    public void setPosition(float x, float y){
+        transformComponent.x = x;
+        wallRectangle.setX(x);
+        transformComponent.y = y;
+        wallRectangle.setY(y);
+    }
+
+    public void increaseX(float increaseValue){
+        transformComponent.x += increaseValue;
+        wallRectangle.setX(transformComponent.x);
     }
 
     @Override
